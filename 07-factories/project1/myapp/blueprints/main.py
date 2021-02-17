b521a9e94ac1f1d1917c1b005b7672fb7291f241
Reinhardz <chonlawit.kingz@g.swu.ac.th>
2021-02-17 10:09:26 +0700

    1

---
+++ 07-factories/project1/myapp/blueprints/main.py
@@ -0,0 +1,10 @@
+from flask import current_app, Blueprint, render_template
+
+main = Blueprint('main', __name__)
+
+@main.route('/')
+def index():
+    return render_template(current_app.config['INDEX_TEMPLATE'])
+
+
+
