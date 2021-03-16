24bb7ffd2afb715e1c64789558a3465e9fa27f31
Reinhardz <chonlawit.kingz@g.swu.ac.th>
2021-03-16 15:06:25 +0700

    3/16/2021

--- Project_Midterm/app.py
+++ Project_Midterm/app.py
@@ -12,36 +12,19 @@ import socket
 
 app = Flask(__name__)
 
-WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather?q={0}&units=metric&APPID={1}"
-WEATHER_KEY = '0edc7b7e529ee3984f27cf41dea2de4f'
 
-NEWS_URL = "https://newsapi.org/v2/top-headlines?q={0}&apiKey={1}"
-NEWS_KEY = 'ebcd985b9a2d4aefb0e99d7b45bf8b3d'
+
+
 
 @app.route('/')
 @app.route('/index')
 def index():
-    city = request.args.get('city')
-
-    if not city:
-        res = requests.get('https://ipinfo.io/')
-        city = res.json()['city']
-    
-    news = getCovidNews()
-
-    weather = get_weather(city,WEATHER_KEY)
-    return render_template('index.html',weather=weather,news=news)
+    return render_template('index.html')
 
 
 @app.route('/genre')
 def news():
-    tag = request.args.get('search_news')
-
-    if not tag:
-        tag = 'phone'
-
-    news = searchNews(tag,NEWS_KEY)
-    return render_template('genre.html', news=news)
+    return render_template('genre.html')
 
 
 
@@ -71,43 +54,6 @@ def getCovidNews():
         list_news.append(news['articles'][i])
     
     return list_news
-
-
-
-
-def get_weather(city,API_KEY):
-    query = quote(city)
-    print(query)
-    url = WEATHER_URL.format(query, API_KEY)
-    print(url)
-    data = urlopen(url).read()
-    parsed = json.loads(data)
-    weather = None
-
-    if parsed.get('weather'):
-
-        description = parsed['weather'][0]['description']
-        temperature = parsed['main']['temp']
-        city = parsed['name']
-        icon = parsed['weather'][0]['icon']
-        country = parsed['sys']['country']
-        wind = parsed['wind']['speed']
-        humidity = parsed['main']['humidity']
-        pressure = parsed['main']['pressure']
-        
-        weather = {'description': description,
-                    'temperature': temperature,
-                    'city': city,
-                    'country': country,
-                    'icon': icon,
-                    'wind': wind,
-                    'humidity': humidity,
-                    'pressure': pressure
-                    }
-
-    return weather
-
-
 
 if __name__ == '__main__':
     app.run(debug=True,use_reloader=True)
